91ae151d3b375fd6e10c7d9131066e7a8e16ddd2
vedangkoolkarni <vedangkoolkarni@gmail.com>
2023-02-17 10:53:55 +0530

    basic scaffolding

--- src/features/about-us/components/about-us.tsx
+++ src/features/about-us/components/about-us.tsx
@@ -7,8 +7,14 @@ import { HomeRenosSection } from './HomeRenosSection/HomeRenosSection';
 import { HowWeStarted } from './HowWeStarted/HowWeStarted';
 import { StartUpSection } from './StartUpSection/StartUpSection';
 import { VideoSection } from './VideoSection/VideoSection';
+// import { useTranslation } from 'react-i18next';
 
 export const AboutUs: React.FC = () => {
+  // TYPOGRAPHY AND TRANSLATION EXAMPLE
+  // const { t } = useTranslation();
+  // <Typography variant="h3" fontWeight="400" component="span" color="primary">
+  //   {t('AboutUs.with_xyz')}
+  // </Typography>
   return (
     <Box p={0} m={0}>
       <HeroSection />
